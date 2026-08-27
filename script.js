944f024bed02d5b1ec41058f84a8c3da2e37b56a
aintharada08-sudo <aintharada08@gmail.com>
2026-08-27 14:17:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
+    document.getElemenById('year').innerHTML = new Date().getFullYear();
     console.log('porfolio ของ AINTHARADA พร้อมแล้ว!');
 });
